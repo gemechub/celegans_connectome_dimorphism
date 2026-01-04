08370c6ca7b6f5589f32dd3e2cdf5f37c7f9a2ba
Gemechu Bekele <gemechu@gemechus-imac.dhcp.wustl.edu>
2026-01-03 18:39:53 -0600

    more cell info is added when extracting functional cartography

--- scripts/functional_carto_features.m
+++ scripts/functional_carto_features.m
@@ -18,13 +18,38 @@ function com_features = functional_carto_features(AM_table, NC, cell_features)
     % Get the neurons features
     [~,idx] = ismember(neurons, cell_features.cell_name);
     cell_type = cell_features.cell_type(idx,:);
+    cell_sex = cell_features.Gender(idx,:);
 
     %% calculate the within-module degree z-score and participation coefficient
     module_zscore = module_degree_zscore(AM, sorted_com_vector, 3);
     participation_coefficient = participation_coef(AM, sorted_com_vector, 1);
 
-    com_features = table(neurons, cell_type, module_zscore, participation_coefficient);
-    
+    %% For each cell, assign region in the functional cartography 
+    region = zeros(length(module_zscore),1);
+    for i=1:length(module_zscore)
+        modz = module_zscore(i);
+        partcoef = participation_coefficient(i);
+        if partcoef<0.05 && modz<2.5
+            r = 1;
+        elseif 0.05<=partcoef && partcoef<0.625 && modz<2.5
+            r = 2;
+        elseif 0.625<= partcoef && partcoef<0.8 && modz<2.5
+            r = 3;
+        elseif partcoef>=0.8 && modz<2.5
+            r = 4;
+        elseif partcoef<0.3 && modz>=2.5
+            r = 5;
+        elseif 0.3<=partcoef && partcoef<0.75 && modz>=2.5
+            r = 6;
+        elseif partcoef>=0.75 && modz>=2.5
+            r = 7;
+        end
+
+        region(i)=r;
+    end
 
 
+    %% make the final table
+    com_features = table(neurons, cell_type, cell_sex, module_zscore, participation_coefficient, region);
+  
 end
